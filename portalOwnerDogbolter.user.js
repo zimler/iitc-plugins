44cbba30cdc442ceb5c09c287ff7a5bf976f0ea4
zimler <zimler@users.noreply.github.com>
2015-07-28 15:42:18 -0500

    Update portalOwnerDogbolter.user.js

--- portalOwnerDogbolter.user.js
+++ portalOwnerDogbolter.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @id             iitc-plugin-highlight-dogbolter@bowering.org
-// @name           IITC plugin: dogbolter portals. Hightlight dogbolter owned portals
+// @id             iitc-plugin-highlight-NoRtH3rnOuTLaW@bowering.org
+// @name           IITC plugin: NoRtH3rnOuTLaW portals. Hightlight NoRtH3rnOuTLaW owned portals
 // @category       Highlighter
 // @version        0.2.0.4
 // @namespace      https://github.com/gb96/iitc-plugins
-// @updateURL      https://github.com/gb96/iitc-plugins/portalOwnerDogbolter.user.js
-// @downloadURL    https://github.com/gb96/iitc-plugins/portalOwnerDogbolter.user.js
-// @description    Use the portal fill color to denote if the portal is owned by dogbolter.
+// @updateURL      https://github.com/gb96/iitc-plugins/portalOwnerNoRtH3rnOuTLaW.user.js
+// @downloadURL    https://github.com/gb96/iitc-plugins/portalOwnerNoRtH3rnOuTLaW.user.js
+// @description    Use the portal fill color to denote if the portal is owned by NoRtH3rnOuTLaW.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
@@ -21,18 +21,18 @@
 // PLUGIN START ////////////////////////////////////////////////////////
 
 // use own namespace for plugin
-window.plugin.dogbolter = function() {};
+window.plugin.NoRtH3rnOuTLaW = function() {};
 
-window.plugin.dogbolter.highlight = function(data) {
+window.plugin.NoRtH3rnOuTLaW.highlight = function(data) {
 
-    console.log("dogbolter highlight called on guid=" + data.portal.options.guid);
+    console.log("NoRtH3rnOuTLaW highlight called on guid=" + data.portal.options.guid);
     if (!data.portal.owner) {
         console.log("placeholder guid=" + data.portal.options.guid);
         var detail = window.portalDetail.get(data.portal.options.guid);
         if (detail) {
             console.log("got detail for guid=" + data.portal.options.guid + " owner=" + detail.owner);
-            if (detail.owner === 'dogbolter') {
-                console.log("found a dogbolter portal");
+            if (detail.owner === 'NoRtH3rnOuTLaW') {
+                console.log("found a NoRtH3rnOuTLaW portal");
                 data.portal.options.fillColor = 'pink';
                 data.portal.options.fillOpacity = '1.0';
             }
@@ -43,15 +43,15 @@ window.plugin.dogbolter.highlight = function(data) {
         }
         return;
     }
-    if (data.owner === 'dogbolter' || data.portal.owner === 'dogbolter') {
-        console.log("found a dogbolter portal");
+    if (data.owner === 'NoRtH3rnOuTLaW' || data.portal.owner === 'NoRtH3rnOuTLaW') {
+        console.log("found a NoRtH3rnOuTLaW portal");
         data.portal.options.fillColor = 'pink';
         data.portal.options.fillOpacity = '1.0';
     }
 }
 
 var setup =  function() {
-    window.addPortalHighlighter('dogbolter Portals', window.plugin.dogbolter.highlight);
+    window.addPortalHighlighter('NoRtH3rnOuTLaW Portals', window.plugin.NoRtH3rnOuTLaW.highlight);
 }
 
 // PLUGIN END //////////////////////////////////////////////////////////
